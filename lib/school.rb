4baa55a01f941b6db15bcf5e6ca24fc178659b03
Michael R <mruhaak@gmail.com>
2020-07-10 15:49:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,15 @@
 class School
+  attr_accessor
+  attr_reader
+
+ROSTER = []
 
   def initialize(name)
       @name = name
     end
+
+def add_student(name, grade)
+end
 end
 
 school = School.new("Bayside High School")
